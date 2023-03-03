fc5d3312635762a7387486ec1e80a080e314754c
alberto <alberto@io.builders>
2023-03-03 11:34:05 +0100

    initial test
    Signed-off-by: alberto <alberto@io.builders>

---
+++ contracts/contracts/echidna/HederaERC20Test.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import '../HederaERC20.sol';
+
+contract HederaERC20Test is HederaERC20 {
+    function echidna_check_balance() public returns (bool) {
+        return (true);
+    }
+}
