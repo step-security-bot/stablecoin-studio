7ce54a5fb1d3baf5658dd25e002c24ace110b41d
alberto <alberto@io.builders>
2023-03-28 10:03:37 +0200

    commit
    Signed-off-by: alberto <alberto@io.builders>

--- contracts/contracts/echidna/HederaERC20Test.sol
+++ contracts/contracts/echidna/HederaERC20Test.sol
@@ -4,11 +4,9 @@ pragma solidity 0.8.16;
 import '../HederaERC20.sol';
 
 contract HederaERC20Test is HederaERC20 {
-    constructor() public {
-        super();
-    }
+    constructor() HederaERC20() {}
 
-    function echidna_check_balance() public returns (bool) {
+    function echidna_check_balance() public pure returns (bool) {
         return (true);
     }
 }
