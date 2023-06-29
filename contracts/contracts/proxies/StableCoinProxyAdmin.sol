45b0f0ed212450fa9d03b1d30d5c556c24ddba90
alberto <alberto@io.builders>
2023-06-29 11:23:20 +0200

    contracts upgrade with TimeLock
    Signed-off-by: alberto <alberto@io.builders>

--- contracts/contracts/proxies/StableCoinProxyAdmin.sol
+++ contracts/contracts/proxies/StableCoinProxyAdmin.sol
@@ -19,6 +19,8 @@ contract StableCoinProxyAdmin is Ownable, ProxyAdmin, Ownable2Step, TimeDelay {
 
     // pending implementation changes
     address private _pendingImplementation;
+    bytes private _pendingImplementationData;
+    uint256 private _pendingImplementationValue;
     ITransparentUpgradeableProxy private _pendingImplementationProxy;
     uint256 private _pendingImplementationTimestamp;
 
@@ -38,12 +40,16 @@ contract StableCoinProxyAdmin is Ownable, ProxyAdmin, Ownable2Step, TimeDelay {
     event ImplementationChangedStarted(
         ITransparentUpgradeableProxy indexed proxy,
         address previousImplementation,
-        address newImplementation
+        address newImplementation,
+        uint256 value,
+        bytes data
     );
     event ImplementationChangedCancelled(
         ITransparentUpgradeableProxy indexed proxy,
         address previousImplementation,
-        address newImplementation
+        address newImplementation,
+        uint256 value,
+        bytes data
     );
 
     constructor(address initialOwner, uint256 initialDelay) Ownable2Step() {
@@ -78,6 +84,14 @@ contract StableCoinProxyAdmin is Ownable, ProxyAdmin, Ownable2Step, TimeDelay {
     }
 
     // Update Admin change
+    function getPendingAdminChange()
+        external
+        view
+        returns (ITransparentUpgradeableProxy, address)
+    {
+        return (_pendingAdminProxy, _pendingAdmin);
+    }
+
     function changeProxyAdmin(
         ITransparentUpgradeableProxy proxy,
         address newAdmin
@@ -111,5 +125,97 @@ contract StableCoinProxyAdmin is Ownable, ProxyAdmin, Ownable2Step, TimeDelay {
         delete (_pendingAdmin);
         delete (_pendingAdminProxy);
         delete (_pendingAdminTimestamp);
+    }
+
+    // Update Implementation change
+    function getPendingImplementationChange()
+        external
+        view
+        returns (ITransparentUpgradeableProxy, address, uint256, bytes memory)
+    {
+        return (
+            _pendingImplementationProxy,
+            _pendingImplementation,
+            _pendingImplementationValue,
+            _pendingImplementationData
+        );
+    }
+
+    function upgrade(
+        ITransparentUpgradeableProxy proxy,
+        address implementation
+    ) public override(ProxyAdmin) onlyOwner {
+        _setNewImplementation(proxy, implementation, 0, '');
+    }
+
+    function upgradeAndCall(
+        ITransparentUpgradeableProxy proxy,
+        address implementation,
+        bytes memory data
+    ) public payable override(ProxyAdmin) onlyOwner {
+        _setNewImplementation(proxy, implementation, msg.value, data);
+    }
+
+    function _setNewImplementation(
+        ITransparentUpgradeableProxy proxy,
+        address implementation,
+        uint256 value,
+        bytes memory data
+    ) private {
+        _pendingImplementation = implementation;
+        _pendingImplementationProxy = proxy;
+        _pendingImplementationValue = value;
+        _pendingImplementationData = data;
+        _pendingImplementationTimestamp = block.timestamp;
+        emit ImplementationChangedStarted(
+            proxy,
+            getProxyImplementation(proxy),
+            implementation,
+            value,
+            data
+        );
+    }
+
+    function cancelUpgradeChange() external onlyOwner {
+        ITransparentUpgradeableProxy proxy = _pendingImplementationProxy;
+        address pendingImplementation = _pendingImplementation;
+        uint256 pendingValue = _pendingImplementationValue;
+        bytes memory pendingData = _pendingImplementationData;
+
+        _removePendingUpgrade();
+
+        emit ImplementationChangedCancelled(
+            proxy,
+            getProxyImplementation(proxy),
+            pendingImplementation,
+            pendingValue,
+            pendingData
+        );
+    }
+
+    function confirmUpgradeChange()
+        external
+        onlyOwner
+        isChangePending(_pendingImplementationTimestamp)
+        isDelayOK(_pendingImplementationTimestamp)
+    {
+        if (
+            _pendingImplementationValue != 0 ||
+            _pendingImplementationData.length > 0
+        )
+            _pendingImplementationProxy.upgradeToAndCall{
+                value: _pendingImplementationValue
+            }(_pendingImplementation, _pendingImplementationData);
+        else _pendingImplementationProxy.upgradeTo(_pendingImplementation);
+
+        _removePendingUpgrade();
+    }
+
+    function _removePendingUpgrade() private {
+        delete (_pendingImplementation);
+        delete (_pendingImplementationProxy);
+        delete (_pendingImplementationValue);
+        delete (_pendingImplementationData);
+        delete (_pendingImplementationTimestamp);
     }
 }
