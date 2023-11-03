592a075204cd48a7c2e86cfc8a5727a533f3015e
Miguel Ángel <miguelangel@io.builders>
2023-11-03 16:43:31 +0100

    Fireblocks signing smart contract call tx working
    Signed-off-by: Miguel Ángel <miguelangel@io.builders>

--- sdk/__tests__/fireblocks/Fireblocks_raw_wipe_fireblocks.test.ts
+++ sdk/__tests__/fireblocks/Fireblocks_raw_wipe_fireblocks.test.ts
@@ -28,7 +28,17 @@ import {
 	TransactionOperation,
 	TransactionStatus,
 } from 'fireblocks-sdk';
-import { Client, TokenId, TokenAssociateTransaction } from '@hashgraph/sdk';
+import Web3 from 'web3';
+import {
+	Client,
+	TokenId,
+	AccountId,
+	ContractExecuteTransaction,
+} from '@hashgraph/sdk';
+import { HederaTokenManager__factory } from '@hashgraph/stablecoin-npm-contracts';
+import BigDecimal from '../../src/domain/context/shared/BigDecimal';
+
+const web3 = new Web3();
 
 const apiSecretKey = fs.readFileSync(
 	path.resolve('/home/mamorales/fireblocks_dario/fireblocks_secret.key'),
@@ -38,8 +48,8 @@ const apiKey = '652415d5-e004-4dfd-9b3b-d93e8fc939d7';
 const baseUrl = 'https://api.fireblocks.io';
 const vaultAccountId = '2';
 
-const tokenHederaId = '0.0.5817119';
-const tokenId = TokenId.fromString(tokenHederaId);
+const nodeId = [];
+nodeId.push(new AccountId(3));
 
 const fireblocksAccountId = '0.0.5712904';
 const fireblocksPublicKey =
@@ -52,9 +62,19 @@ client.setOperatorWith(
 	signingService,
 );
 
-const transaction = new TokenAssociateTransaction()
-	.setAccountId(fireblocksAccountId)
-	.setTokenIds([tokenId]);
+const functionCallParameters = encodeFunctionCall(
+	'wipe',
+	[
+		'0x0000000000000000000000000000000000004719',
+		BigDecimal.fromString('1', 6),
+	],
+	HederaTokenManager__factory.abi,
+);
+
+const transaction = new ContractExecuteTransaction()
+	.setContractId('0.0.5759337')
+	.setFunctionParameters(functionCallParameters)
+	.setGas(80000);
 
 const fireblocks: FireblocksSDK = new FireblocksSDK(
 	apiSecretKey,
@@ -67,6 +87,26 @@ describe('🧪 Firebocks signing a Hedera transaction', () => {
 		await transaction.execute(client);
 	}, 90_000);
 });
+
+function encodeFunctionCall(
+	functionName: string,
+	parameters: any[],
+	abi: any,
+): Uint8Array {
+	const functionAbi = abi.find(
+		(func: { name: any; type: string }) =>
+			func.name === functionName && func.type === 'function',
+	);
+	if (!functionAbi) {
+		const message = `Contract function ${functionName} not found in ABI, are you using the right version?`;
+		throw new Error(message);
+	}
+	const encodedParametersHex = web3.eth.abi
+		.encodeFunctionCall(functionAbi, parameters)
+		.slice(2);
+
+	return Buffer.from(encodedParametersHex, 'hex');
+}
 
 async function signingService(
 	transactionBytes: Uint8Array,
