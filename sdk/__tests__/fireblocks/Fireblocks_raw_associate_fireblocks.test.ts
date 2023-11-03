c89b306ae0403235471fb742858e16d02876e628
Miguel Ángel <miguelangel@io.builders>
2023-11-03 13:27:56 +0100

    Fireblocks signing a native hedera tx working
    Signed-off-by: Miguel Ángel <miguelangel@io.builders>

--- sdk/__tests__/fireblocks/Fireblocks_raw_associate_fireblocks.test.ts
+++ sdk/__tests__/fireblocks/Fireblocks_raw_associate_fireblocks.test.ts
@@ -32,70 +32,65 @@ import {
 import {
 	Client,
 	TokenId,
-	PublicKey,
 	AccountId,
 	TokenAssociateTransaction,
 } from '@hashgraph/sdk';
 
+const apiSecretKey = fs.readFileSync(
+	path.resolve('/home/mamorales/fireblocks_dario/fireblocks_secret.key'),
+	'utf8',
+);
+const apiKey = '652415d5-e004-4dfd-9b3b-d93e8fc939d7';
+const baseUrl = 'https://api.fireblocks.io';
+const vaultAccountId = '2';
+
+const tokenHederaId = '0.0.5759338';
+const tokenId = TokenId.fromString(tokenHederaId);
+
+const nodeId = [];
+nodeId.push(new AccountId(3));
+
+const fireblocksAccountId = '0.0.5712904';
+const fireblocksPublicKey =
+	'04eb152576e3af4dccbabda7026b85d8fdc0ad3f18f26540e42ac71a08e21623';
+
+const client = Client.forTestnet();
+client.setOperatorWith(
+	fireblocksAccountId,
+	fireblocksPublicKey,
+	signingService,
+);
+client.setMaxNodesPerTransaction(1);
+
+const transaction = new TokenAssociateTransaction()
+	.setNodeAccountIds(nodeId)
+	.setAccountId(fireblocksAccountId)
+	.setTokenIds([tokenId])
+	.freezeWith(client);
+
+const fireblocks: FireblocksSDK = new FireblocksSDK(
+	apiSecretKey,
+	apiKey,
+	baseUrl,
+);
+
 describe('🧪 Firebocks signing a Hedera transaction', () => {
-	const operatorAccountHederaId = '0.0.18201';
-	const operatorPrivateKey =
-		'302e020100300506032b657004220420f6392a8242bce3be5bf69fc607a153e65c99bf4b39126f1d41059b00c49ee318';
-
-	const signerPublicKey =
-		'04eb152576e3af4dccbabda7026b85d8fdc0ad3f18f26540e42ac71a08e21623';
-
-	const client = Client.forTestnet();
-	client.setOperator(operatorAccountHederaId, operatorPrivateKey);
-	const accountId = '0.0.5712904';
-	const vaultAccountId = '2';
-
-	const apiSecretKey = fs.readFileSync(
-		path.resolve('/home/mamorales/fireblocks_dario/fireblocks_secret.key'),
-		'utf8',
-	);
-	const apiKey = '652415d5-e004-4dfd-9b3b-d93e8fc939d7';
-	const baseUrl = 'https://api.fireblocks.io';
-	const fireblocks: FireblocksSDK = new FireblocksSDK(
-		apiSecretKey,
-		apiKey,
-		baseUrl,
-	);
-
-	const tokenHederaId = '0.0.5759338';
-	const tokenId = TokenId.fromString(tokenHederaId);
-
-	const nodeId = [];
-	nodeId.push(new AccountId(3));
-
-	const transaction = new TokenAssociateTransaction()
-		.setNodeAccountIds(nodeId)
-		.setAccountId(accountId)
-		.setTokenIds([tokenId])
-		.freezeWith(client);
-
 	it('Signing a raw transaction', async () => {
-		const serializedTransaction = Buffer.from(
-			transaction.toBytes(),
-		).toString('hex');
-
-		const signatureHex = await signArbitraryMessage(
-			fireblocks,
-			vaultAccountId,
-			serializedTransaction,
-		);
-
-		const signature = hexStringToUint8Array(signatureHex);
-
-		const publicKey = PublicKey.fromString(signerPublicKey);
-		const signedTransaction = transaction.addSignature(
-			publicKey,
-			signature,
-		);
-
-		await signedTransaction.execute(client);
+		await transaction.execute(client);
 	}, 90_000);
 });
+
+async function signingService(
+	transactionBytes: Uint8Array,
+): Promise<Uint8Array> {
+	const serializedTransaction = Buffer.from(transactionBytes).toString('hex');
+	const signatureHex = await signArbitraryMessage(
+		fireblocks,
+		vaultAccountId,
+		serializedTransaction,
+	);
+	return hexStringToUint8Array(signatureHex);
+}
 
 function hexStringToUint8Array(hexString: string): Uint8Array {
 	const uint8Array = new Uint8Array(hexString.length / 2);
@@ -104,7 +99,6 @@ function hexStringToUint8Array(hexString: string): Uint8Array {
 		const byte = parseInt(hexString.substr(i, 2), 16);
 		uint8Array[i / 2] = byte;
 	}
-
 	return uint8Array;
 }
 
@@ -113,9 +107,6 @@ async function signArbitraryMessage(
 	vaultAccountId: string,
 	message: string,
 ): Promise<string> {
-	const hash = createHash('sha256').update(message, 'utf8').digest();
-	const content = createHash('sha256').update(hash).digest('hex');
-
 	const { status, id } = await fireblocks.createTransaction({
 		operation: TransactionOperation.RAW,
 		assetId: 'HBAR_TEST',
@@ -127,7 +118,7 @@ async function signArbitraryMessage(
 			rawMessageData: {
 				messages: [
 					{
-						content,
+						content: message,
 					},
 				],
 			},
@@ -152,8 +143,6 @@ async function signArbitraryMessage(
 		}
 		await new Promise((r) => setTimeout(r, 1000));
 	}
-
 	const signature = txInfo.signedMessages[0].signature;
-	console.log('signed message: ' + JSON.stringify(txInfo.signedMessages[0]));
 	return signature.fullSig;
 }
